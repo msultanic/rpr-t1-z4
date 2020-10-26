26d7e715607f494e25bee7c10ec4cab328301ecf
Muaz Sultanić <msultanic1@etf.unsa.ba>
2020-10-26 19:42:40 +0100

    ispravljene minorne greške

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -2,8 +2,11 @@ package ba.unsa.etf.rpr;
 
 public class Korpa {
     Artikl popis[] = new Artikl[50];
+    int ukupno=0;
 
     public boolean dodajArtikl(Artikl a) {
+        popis[ukupno]=a;
+        ukupno++;
         return true;
     }
 
@@ -12,10 +15,20 @@ public class Korpa {
     }
 
     public Artikl izbaciArtiklSaKodom(String kod) {
-        return popis[0];
+        Artikl vrati=null;
+        for(int i=0; i<ukupno; i++){
+            if(popis[i].getKod()==kod)
+                vrati=popis[i];
+            //dodati izbacivanje i provjeriti zašto ne radi kako treba
+        }
+        ukupno--;
+        return vrati;
     }
 
     public int dajUkupnuCijenuArtikala() {
-        return 10;
+        int suma=0;
+        for(int i=0; i<ukupno; i++)
+            suma+=popis[i].getCijena();
+        return suma;
     }
 }
