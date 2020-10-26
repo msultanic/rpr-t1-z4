ff58c7efe917283adec0940bcdcd47b103f1b255
Muaz Sultanić <msultanic1@etf.unsa.ba>
2020-10-26 19:26:23 +0100

    nastavljamo kasnije, ispravljene osnovne metode

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,12 +1,31 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+    String naziv;
+    String kod;
+    String cijena;
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public void setKod(String kod) {
+        this.kod = kod;
+    }
+
+    public void setCijena(String cijena) {
+        this.cijena = cijena;
+    }
+
     public String getNaziv() {
+        return naziv;
     }
 
     public String getKod() {
+        return kod;
     }
 
     public String getCijena() {
+        return cijena;
     }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,12 +1,28 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    Artikl proizvodi[] = new Artikl[];
+    int ukupno = 0;
     public Artikl[] getArtikli() {
+        return proizvodi;
     }
 
     public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl izbacit=null;
+        for(int i = 0; i<ukupno; i++)
+            if(proizvodi[i].getKod()==kod){
+                izbacit=proizvodi[i];
+                ukupno--;
+                for(int j=i; j<ukupno-1; j++)
+                    proizvodi[j]=proizvodi[j+1];
+            }
+        return izbacit;
     }
 
     public void dodajArtikl(Artikl a) {
+        proizvodi[ukupno].setCijena(a.getCijena());
+        proizvodi[ukupno].setKod(a.getKod());
+        proizvodi[ukupno].setNaziv(a.getNaziv());
+        ukupno++;
     }
 }
